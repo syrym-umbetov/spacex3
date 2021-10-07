979c02ffa8b5eeb7bc9a360411507e36e758246d
syrym-umbetov <umbetov.syrym@gmail.com>
2021-10-07 17:43:24 +0600

    Past launches

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { ApolloProvider, InMemoryCache, ApolloClient } from '@apollo/client';
 import Routing from './routes/index';
-import logo from './logo.svg';
-import './App.css';
 
 const client = new ApolloClient({
   uri: 'https://api.spacex.land/graphql',
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,7 +1,51 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { gql, useQuery } from '@apollo/client';
+import Error from './../Error'
+import Loader from './../Loader'
+import TopMenu from './../TopMenu';
+import SideMenu from './../SideMenu';
+import useNavigation from './../../hooks/useNavigation';
+import './style.scss';
 
+
+const GET_ROCKETS_NAMES = gql`
+    {
+        rockets(offset: 1) {
+            id
+            name
+        }
+    }
+`;
 const NavBar = () => {
-    return null;
+    const NavRef = useRef(null);
+    const {
+        isMobileView,
+        isMenuOpen,
+        setIsMenuOpen,
+    } = useNavigation(NavRef);
+    const { data, loading, error } = useQuery(GET_ROCKETS_NAMES);
+    console.log("data:check", data)
+    if (loading) return <Loader />
+    if (error) return <Error error={error} />
+    
+    return (
+    <div className="container-fluid" ref = {NavRef}>
+            <div className="row">
+                <TopMenu
+                    isMenuOpen={isMenuOpen}
+                    isMobileView={isMobileView}
+                    toggleMenu={setIsMenuOpen}
+                    rockets={data.rockets}
+                />    
+                <SideMenu
+                    isMenuOpen={isMenuOpen}
+                    isMobileView={isMobileView}
+                    toggleMenu={setIsMenuOpen}
+                    rockets={data.rockets}
+                />    
+            </div>
+    </div>
+    );
 };
 
 export default NavBar;
