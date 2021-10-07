ea298f5a529ae5625c64bce9d2f75f55365c833e
syrym-umbetov <umbetov.syrym@gmail.com>
2021-10-07 14:23:00 +0600

    connect to spacex API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
+import React from 'react';
+import { ApolloProvider, InMemoryCache, ApolloClient } from '@apollo/client';
+import Routing from './routes/index';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const client = new ApolloClient({
+  uri: 'https://api.spacex.land/graphql',
+  cache: new InMemoryCache(),
+});
+
+const App = () => (
+  <ApolloProvider client={client}>
+      <Routing />
+  </ApolloProvider>
+);
 
 export default App;
